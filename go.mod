4520c1fb96d8eeed14328719b9b337ac78ddaa1f
Franciszek <fr.strzezek@gmail.com>
2024-08-04 18:28:27 +0200

    add curl to pea transform

--- go.mod
+++ go.mod
@@ -6,26 +6,21 @@ require (
 	github.com/charmbracelet/bubbles v0.18.0
 	github.com/charmbracelet/bubbletea v0.26.4
 	github.com/charmbracelet/lipgloss v0.9.1
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-runewidth v0.0.15
-	github.com/redis/go-redis/v9 v9.5.3
 	github.com/stretchr/testify v1.9.0
-	github.com/valyala/fasthttp v1.55.0
 	golang.org/x/exp v0.0.0-20220909182711-5c715a9e8561
 )
 
 require (
-	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/charmbracelet/x/ansi v0.1.2 // indirect
 	github.com/charmbracelet/x/input v0.1.0 // indirect
 	github.com/charmbracelet/x/term v0.1.1 // indirect
 	github.com/charmbracelet/x/windows v0.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -35,7 +30,6 @@ require (
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/fr-str/httpea/internal/config"
 	"github.com/fr-str/httpea/pkg/tui"
+	"github.com/google/shlex"
 )
 
 func main() {
+	curl := flag.String("curl", "", "transform curl command to pea format")
+	flag.Parse()
+
 	if config.Debug {
 		f, err := tea.LogToFile("debug.log", "debug")
 		if err != nil {
@@ -18,6 +25,12 @@ func main() {
 		}
 		defer f.Close()
 		config.DebugLogFile = f
+		// config.DebugLogFile = os.Stdout
+	}
+
+	if *curl != "" {
+		parseCurl(*curl)
+		return
 	}
 
 	startTUI()
@@ -36,4 +49,124 @@ func startTUI() {
 		fmt.Printf("there's been an error: %v", err)
 		os.Exit(1)
 	}
+}
+
+func parseCurl(curl string) {
+	pc, err := Parse(curl)
+	if err != nil {
+		panic(err)
+	}
+	transformToPea(pc)
+}
+
+func transformToPea(pc *parsedCurl) {
+	buf := strings.Builder{}
+	buf.WriteString("[Host]\n")
+	buf.WriteString(pc.Url)
+	buf.WriteString("\n")
+	buf.WriteString("\n")
+	buf.WriteString("[Method]\n")
+	buf.WriteString(pc.Method)
+	buf.WriteString("\n")
+	buf.WriteString("\n")
+	buf.WriteString("[Headers]\n")
+	for k, v := range pc.Headers {
+		buf.WriteString(k)
+		buf.WriteString(": ")
+		buf.WriteString(v)
+		buf.WriteString("\n")
+	}
+	buf.WriteString("\n")
+	buf.WriteString("[Body]\n")
+	buf.WriteString(pc.Body)
+	fmt.Println(buf.String())
+}
+
+type parsedCurl struct {
+	Method  string
+	Url     string
+	Body    string
+	Headers map[string]string
+}
+
+func Parse(curl string) (*parsedCurl, error) {
+	pc := &parsedCurl{Headers: map[string]string{}}
+
+	parts, err := shlex.Split(curl)
+	if err != nil {
+		return nil, err
+	}
+
+	var trimmedParts []string
+	for _, p := range parts {
+		part := strings.TrimSpace(p)
+		if part != "" {
+			trimmedParts = append(trimmedParts, part)
+		}
+	}
+
+	var currentPart, nextPart string
+	for i := 1; i < len(trimmedParts); {
+		currentPart = trimmedParts[i-1]
+		nextPart = trimmedParts[i]
+		if currentPart != "" {
+			switch currentPart {
+			case "-X":
+				pc.Method = strings.ToUpper(nextPart)
+				i++
+			case "-H":
+				k, v, err := parseHeader(nextPart)
+				if err != nil {
+					return nil, err
+				}
+				pc.Headers[strings.ToLower(k)] = v
+				i++
+			case "-d":
+				pc.Body = nextPart
+				i++
+			case "--data-raw":
+				pc.Body = nextPart
+				i++
+			case "--abstract-unix-socket":
+				i++
+			case "--alt-svc":
+				i++
+			case "--aws-sigv4":
+				i++
+			case "-a":
+			case "--append":
+			case "--anyauth":
+			case "--basic":
+			case "curl":
+			case "-k":
+			case "-v":
+			case "-V":
+			default:
+				if !strings.HasPrefix(currentPart, "-") {
+					pc.Url = currentPart
+				}
+			}
+		}
+		i++
+	}
+
+	if pc.Body != "" {
+		if pc.Method == "" {
+			pc.Method = "POST"
+		}
+	} else {
+		if pc.Method == "" {
+			pc.Method = "GET"
+		}
+	}
+
+	return pc, nil
+}
+
+func parseHeader(h string) (string, string, error) {
+	parts := strings.SplitN(h, ":", 2)
+	if len(parts) == 2 {
+		return strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1]), nil
+	}
+	return "", "", errors.New(fmt.Sprintf(`wrong header format: %v`, h))
 }
