6d09ab5c58a7d6f86cb0898d6ab976b827294479
Franciszek <fr.strzezek@gmail.com>
2024-09-17 00:51:44 +0200

    drop json color

--- pkg/tui/view.go
+++ pkg/tui/view.go
@@ -1,14 +1,11 @@
 package tui
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
-	"regexp"
 	"time"
 
 	"github.com/charmbracelet/lipgloss"
-	"github.com/fr-str/httpea/internal/log"
-	"github.com/fr-str/httpea/internal/util"
 )
 
 var (
@@ -38,10 +35,13 @@ func (m model) View() string {
 		lenDur = len(duration) - 3
 	}
 
-	// colour JSON
 	if m.ReqView.header != nil && m.ReqView.header.Get("Content-Type") == "application/json" {
-		m.ReqView.SetContent(FixedJQ(m.ReqView.Body, m.ReqView.Width-1))
-		// m.ReqView.SetContent(hackedJsonColorizer(m.ReqView.Body, m.ReqView.Width-1))
+		b, err := json.MarshalIndent(json.RawMessage(m.ReqView.Body), "", "  ")
+		if err != nil {
+			m.ReqView.SetContent(fmt.Sprintf("Error: %s", err))
+		} else {
+			m.ReqView.SetContent(string(b))
+		}
 	} else {
 		m.ReqView.SetContent(m.ReqView.Body)
 	}
@@ -59,93 +59,6 @@ func (m model) View() string {
 	help := m.help.View(m.keys)
 
 	return lipgloss.JoinHorizontal(lipgloss.Top, views...) + "\n" + help
-}
-
-var (
-	resetColor         = "\033[0m"
-	regANSIIColor      = util.Must(regexp.Compile(`\033\[[0-9];[0-9]+m`))
-	regANSIIResetColor = util.Must(regexp.Compile(`\033\[0m`))
-)
-
-func FixedJQ(s string, limit int) string {
-	b, _ := util.PrettyJSON([]byte(s))
-
-	ret := bytes.NewBuffer(make([]byte, 0, len(b)))
-	for _, l := range bytes.Split(b, []byte("\n")) {
-		f := regANSIIColor.FindAllIndex(l, -1)
-		if len(f) == 0 {
-			continue
-		}
-		colors := regANSIIColor.FindAll(l, -1)
-		color := []byte{}
-		if len(colors) == 1 {
-			color = colors[0]
-		} else {
-			color = colors[len(colors)-2]
-		}
-
-		lastIdx := f[len(f)-1][1]
-		// ignore escape codes in line wreapping
-		tmp := regANSIIColor.ReplaceAll(l, []byte{})
-		tmp = regANSIIResetColor.ReplaceAll(tmp, []byte{})
-		if len(tmp) > limit {
-			writeWithLimit(ret, string(l[:lastIdx]), string(color), limit)
-		} else {
-			ret.WriteString(fmt.Sprintf("%s%s%s", color, l[:lastIdx], resetColor))
-		}
-		ret.Write(l[lastIdx:])
-		ret.Write([]byte(resetColor))
-		ret.WriteString("\n")
-	}
-
-	// return ""
-	return ret.String()
-
-}
-
-func writeWithLimit(w *bytes.Buffer, s string, color string, limit int) {
-	tmp := s
-	space := spceLen(s)
-	first := true
-	for len(removeANSII(tmp)) > limit {
-		ansiL := ansiiLen(tmp)
-		log.Debug("ansiL", ansiL)
-		line := tmp[:limit+ansiL]
-		if !first {
-			line = space + line
-		}
-		first = false
-		w.WriteString(fmt.Sprintf("%s%s%s\n", color, line, resetColor))
-		tmp = tmp[len(line):]
-		log.Debug("[dupa] tmp: ", tmp)
-	}
-	w.WriteString(fmt.Sprintf("%s%s%s", color, space+tmp, resetColor))
-}
-
-func removeANSII(s string) string {
-	s = string(regANSIIColor.ReplaceAll([]byte(s), []byte{}))
-	return string(regANSIIResetColor.ReplaceAll([]byte(s), []byte{}))
-}
-
-func ansiiLen(s string) int {
-	f1 := regANSIIColor.FindAllString(s, -1)
-	f2 := regANSIIResetColor.FindAllString(s, -1)
-	count := 0
-	for _, v := range f1 {
-		count += len(v)
-	}
-	for _, v := range f2 {
-		count += len(v)
-	}
-	return count
-}
-
-func spceLen(s string) string {
-	count := 0
-	for i := 0; s[i] == ' '; i++ {
-		count++
-	}
-	return s[:count]
 }
 
 // lol works
